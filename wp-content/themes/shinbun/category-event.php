be9064770a1d93a5d3a5e5eee653903936f34b7f
Takahisa Yoshimoto <takahisa86@gmail.com>
2017-07-26 22:54:52 +0900

    v1.21_commit

--- wp-content/themes/shinbun/category-event.php
+++ wp-content/themes/shinbun/category-event.php
@@ -47,33 +47,37 @@ get_header(); ?>
       <div class="col-sm-9 col-xs-12">
         <div class="border">
         </div>
-          <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
           <a href="<?php the_permalink(); ?>">
-            <div class="event_text col-xs-7">
-              <p><?php the_time('y/m/d'); ?></p>
-  	          <h2>
-                <?php
-                  if(mb_strlen($post->post_title, 'UTF-8')>10){
-                    $content= mb_substr(strip_tags($post->post_title), 0, 10, 'UTF-8');
-                    echo $content.'…';
-                  }else{
-                    echo strip_tags($post->post_title);
-                  }
-                ?>
-              </h2>
-              <p><?php the_excerpt(); ?></p>
-            </div>
-            <div class="event_text_img col-xs-5">
-              <?php if (has_post_thumbnail()) : ?>
-                <?php the_post_thumbnail(array(100,100)); ?>
-              <?php else: ?>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/no_image.png" alt="no image" width="100" height="100">
-              <?php endif; ?>
+            <div class="each_container co-xs-12">
+              <div class="event_text col-xs-7">
+                <p><?php the_time('y/m/d'); ?></p>
+    	          <h2>
+                  <?php
+                    if(mb_strlen($post->post_title, 'UTF-8')>10){
+                      $content= mb_substr(strip_tags($post->post_title), 0, 10, 'UTF-8');
+                      echo $content.'…';
+                    }else{
+                      echo strip_tags($post->post_title);
+                    }
+                  ?>
+                </h2>
+                <p><?php the_excerpt(); ?></p>
+              </div>
+              <div class="event_text_img col-xs-5">
+                <?php if (has_post_thumbnail()) : ?>
+                  <?php the_post_thumbnail(array(100,100)); ?>
+                <?php else: ?>
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/no_image.png" alt="no image" width="100" height="100">
+                <?php endif; ?>
+              </div>
             </div>
           </a>
-          <?php endwhile; else: ?>
-            <?php _e('記事がありません。'); ?>
-          <?php endif; ?>
+          <div class="border">
+          </div>
+        <?php endwhile; else: ?>
+          <?php _e('記事がありません。'); ?>
+        <?php endif; ?>
       </div>
 
 
